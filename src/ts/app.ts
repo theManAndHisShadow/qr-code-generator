6609cdbc41158a9f8d90d7107da7f859c7c181b8
plvs vltra <justemailpostbox@gmail.com>
2023-02-14 17:42:06 +0500

    Basic qr creating controls and output rendering

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,21 @@
 import {qr} from './lib/qr';
 
-const input = document.querySelector('#app input');
+const input = document.querySelector('#app input') as HTMLInputElement;
+const correctionLevelSelector = document.querySelector('#app select') as HTMLSelectElement;
 const triggerButton = document.querySelector('#app button');
-const appResultTargets = document.querySelectorAll('[data-qr-code-container]');
+const appResult = document.querySelector('#app #output') as HTMLDivElement;
 
 triggerButton?.addEventListener('click', () => {
-    if(input) {
-        console.log(input);
+    if(input && input.value.length > 0) {
+        let qrCode = qr({
+            text: input.value,
+            correction: correctionLevelSelector.value,
+        });
+
+        appResult.children.length > 0 
+            ? appResult.replaceChild(qrCode.canvas, appResult.children[0])
+            : appResult.appendChild(qrCode.canvas);
+
+        console.log(qrCode.data);
     }
 });

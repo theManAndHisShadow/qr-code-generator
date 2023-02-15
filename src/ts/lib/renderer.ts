181f6bb4c0adcb8dcec8d91bb6ada6c01dfdb23e
plvs vltra <justemailpostbox@gmail.com>
2023-02-15 19:04:50 +0500

    Added drawModule(), getBoundingRect() and drawFinderPatterns() functions. Module size value now rounded

--- src/ts/lib/renderer.ts
+++ src/ts/lib/renderer.ts
@@ -32,12 +32,97 @@ export function getAlignmentAnchorPos(versionNumber:number){
 export function getModulesAmount(versionNumber: number){
     let aligmenAnchorPos = getAlignmentAnchorPos(versionNumber);
 
-    return aligmenAnchorPos[0] === 0
-        ? 21 : aligmenAnchorPos[aligmenAnchorPos.length - 1] + 7;
+    // + 8: 4 modules width for each side - white space around qr code!
+    return (aligmenAnchorPos[0] === 0
+        ? 21 : aligmenAnchorPos[aligmenAnchorPos.length - 1] + 7) + 8;
 }
+
+
+
+/**
+ * Draw single module (1x1).
+ * @param context canvas contexn
+ * @param x coord
+ * @param y coord
+ * @param size of module
+ * @param color of module (dafault value - black)
+ */
+function drawModule(context: CanvasRenderingContext2D, x: number, y: number, size: number, color?: string) {
+    color = color || 'black';
+
+    context.fillStyle = color;
+    context.fillRect(x, y, size, size);
+}
+
+
+
+/**
+ * Returns bounding rectangle points.
+ * @param context canvas context
+ * @param size of module
+ * @returns 
+ */
+function getBoundingRect(context: CanvasRenderingContext2D, size: number){
+    let w = context.canvas.width;
+    let h = context.canvas.height;
+    let offset = size * 4;
+
+    return {
+        leftTop: [0 + offset, 0 + offset],
+        rightTop: [w - offset, 0 + offset],
+        leftBottom: [0 + offset, h - offset],
+        rightBottom: [w - offset, h - offset],
+    };
+}
+
+
+
+/**
+ * Draws three finder patterns.
+ * @param context canvas 2d context
+ * @param size of module
+ */
+function drawFinderPatterns(context: CanvasRenderingContext2D, size: number){
+    for(let i = 0; i <= 6; i++){
+        for(let j = 0; j <= 6; j++) {
+            let isEdge = ((j === 0 || i === 0) || (j === 6 || i === 6));
+            let isNotWhiteLines = ((i !== 1 && i !== 5) && (j !== 1 && j !== 5));
+            let rect = getBoundingRect(context, size);
+
+            if(isEdge || isNotWhiteLines){
+                // top left pattern
+                drawModule(
+                    context, 
+                    rect.leftTop[0] + i*size, 
+                    rect.leftTop[1] + j*size, 
+                    size
+                );
+
+                // top right pattern
+                drawModule(
+                    context, 
+                    rect.rightTop[0] - (i * size) - size, 
+                    rect.rightTop[1] + j*size, 
+                    size
+                );
+
+                // left bottom pattern
+                drawModule(
+                    context, 
+                    rect.leftBottom[0] + i*size, 
+                    rect.leftBottom[1] - j*size - size, 
+                    size
+                );
+            }
+        } 
+    }
+}
+
+
 
 export function drawQR(canvas: HTMLCanvasElement, data: any){
     let context = canvas.getContext('2d');
-
+    let moduleSize = Math.round(canvas.width / getModulesAmount(data.version.number));
     fillBackground(context);
+    drawFinderPatterns(context,moduleSize);
 }

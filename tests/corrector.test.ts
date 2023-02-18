8b2e1d3ca3dac0cc050a285851b92d47ac77767b
plvs vltra <justemailpostbox@gmail.com>
2023-02-18 23:41:20 +0500

    Updated test case

--- tests/corrector.test.ts
+++ tests/corrector.test.ts
@@ -13,13 +13,13 @@ let preparedData1 = prepareData(text1, 'H');
 let preparedData2 = prepareData(text2, 'Q')
 
 describe('getCorrectionBytesAmount() returns amount of correction bytes that depends on correction level and version number', () => {
-    test('getCorrectionBytesAmount(preparedData1) returns 17', ()=>{
+    test('getCorrectionBytesAmount(preparedData1) returns 28', ()=>{
         expect(
             getCorrectionBytesAmount(
                 preparedData1.correction, 
                 preparedData1.version.number
             )
-        ).toBe(17);
+        ).toBe(28);
     });
 
     test('getCorrectionBytesAmount(preparedData2) returns 28', ()=>{
@@ -97,17 +97,34 @@ describe('calculateCorrectionBytes() returns correction bytes array', () => {
 });
 
 
-
 let grouped1 = divideIntoBlocks(preparedData1);
 let readyDataObject = addCorrectionBytes(grouped1);
 
-// readyDataObject`s bytes
+// readyDataObject`s main and correction bytes
 // ||
 // \/
-let flattedBytes = ['00100001', '11101011','00001011', '01111000','11010001', '01110010','11011100', '01001101','01000011', '01000000'];
-let flattedCorrectionBytes = [
-    '11101001', '10001100','00110010', '10101010','11000101', '10111111','01001000', '11101101',
-    '10001011', '00010100','10111011', '00110101','10101111', '10111111','00101100', '10100011','00100100'
+let flattedBytes = [
+    '00100010', '10000011',
+    '00001011', '01111000',
+    '11010001', '01110010',
+    '11011100', '01001101',
+    '01000011', '01000000',
+    '11101100', '00010001',
+    '11101100', '00010001',
+    '11101100', '00010001'
+];
+
+let flattedCorrectionBytes =     [
+    '00101001', '00011000', '11011100',
+    '00000101', '01001010', '01100101',
+    '00111000', '10000000', '11100001',
+    '10011011', '10010001', '01010011',
+    '01101001', '10111000', '10111001',
+    '11000111', '11001111', '00111001',
+    '11110010', '10000011', '10110110',
+    '11100101', '01001011', '01110001',
+    '11100110', '10111101', '01101111',
+    '10101100'
 ];
 
 describe('addCorrectionBytes() returns new steam with correction bytes', () => {

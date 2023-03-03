6eef0e4df63ce42a7214d32dff6dffa5823e8c32
plvs vltra <justemailpostbox@gmail.com>
2023-03-04 00:33:50 +0500

    Added new functionality for dev block

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,9 +4,19 @@ import { sanitizeInput } from './lib/helper';
 const input = document.querySelector('#controls__inputs input') as HTMLInputElement;
 const correctionLevelSelector = document.querySelector('#controls__inputs select') as HTMLSelectElement;
 const devModeCheckbox = document.querySelector('#dev-mode-checkbox input') as HTMLInputElement;
-const devInfo = document.querySelector('#dev-info') as HTMLDivElement;
+const devInfoContainer = document.querySelector('#dev') as HTMLInputElement;
+const devInfo = document.querySelector('#dev__info') as HTMLDivElement;
 const triggerButton = document.querySelector('#controls__trigger button');
 const appResult = document.querySelector('#app #app__output') as HTMLDivElement;
+
+devModeCheckbox?.addEventListener('click', () => {
+    if(devModeCheckbox.checked){
+        devInfoContainer.classList.remove('non-active');
+    } else {
+        devInfoContainer.classList.add('non-active');
+        devInfo.innerHTML = "";
+    }
+});
 
 triggerButton?.addEventListener('click', () => {
     if(input) {
@@ -29,8 +39,14 @@ triggerButton?.addEventListener('click', () => {
             console.log(qrCode.data);
             
             if(devModeCheckbox.checked){
-                devInfo.innerHTML = JSON.stringify(qrCode.data);
-            } 
+                let devData = {
+                    correction: qrCode.data.correction,
+                    version: qrCode.data.version,
+                }
+                devInfo.innerHTML = JSON.stringify(devData);
+            } else {
+                devInfo.innerHTML = "";
+            }
         }
     }
 });
